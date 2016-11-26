8542dff8209d15eda8e5102eed896ea21a156556
wcarvalho <wcarvalho92@gmail.com>
2016-11-25 17:50:51 -0800

    add cmd ops

--- seq2seq_keras/beam_search.py
+++ seq2seq_keras/beam_search.py
@@ -7,88 +7,6 @@ PI = 0 # probability index
 II = 1 # word index
 NI = 2 # node index
 
-class Graph(object):
-  """docstring for Graph"""
-
-  def __init__(self, max_size=10):
-    self.map = {}
-    self.queue = deque([])
-    self.max_size = max_size
-    self.latest_indx = 0
-
-  def size(self): return len(self.queue)
-  def full(self): return self.size() >= self.max_size
-
-  def add(self, node, parent=None):
-    # increase global index for nodes
-    self.latest_indx +=1
-    node.set_indx(self.latest_indx)
-    if parent is not None:
-      parent = self.map[parent]
-      node.set_parent(parent)
-
-    removed = None
-    if self.full():
-      removed = self.queue.popleft()
-
-    self.map[node.get_indx()] = node
-    self.queue.append((node.get_probability(), node.get_word_indx(), node.get_indx()))
-
-    # if removed EOS check that probability is lower than lowest in current queue. rebuild if not
-    if removed is not None:
-      i = removed[II]
-      p = removed[PI]
-      if i == 2:
-        l = list(self.queue)
-        sorted_queue = sorted(l, key=lambda x: x[PI])
-        lowest_p_in_q = sorted_queue[0][PI]
-        lowest_i_in_q = sorted_queue[0][II]
-        # if p is greater than lowest, rebuild
-        if p >= lowest_p_in_q:
-          new_list = [i for i in self.queue if i[II] != lowest_i_in_q]
-          new_list.append(removed)
-          self.queue = deque(new_list)
-
-
-  def print_map(self):
-    for i in self.map: print self.map[i]
-  
-  def print_best(self):
-    for i in self.get_best_queue(): print self.map[i[NI]]
-
-  # returns a sorted list of values
-  def get_best_queue(self): 
-    l = list(self.queue)
-    sor = sorted(l, key=lambda x: x[PI], reverse=True)
-    return sor[:self.max_size]
-  
-  def get_best(self): 
-    best = self.get_best_queue()
-    indices = [i[NI] for i in best if i[II] != 2]
-    word_indices = [i[II] for i in best if i[II] != 2]
-    probabilities = [i[PI] for i in best if i[II] != 2]
-    weights = [self.map[i[NI]].get_weights() for i in best if i[II] != 2]
-    return indices, word_indices, probabilities, weights
-
-  def get_sequences(self, verbosity):
-    sequences = []
-    best = self.get_best_queue()
-    for i in best:
-      if verbosity > 1: print ""
-      sequence = self.get_sequence(i[NI], verbosity)
-      converted_sequence = [self.map[i].get_word_indx() for i in sequence]
-      sequences.append(converted_sequence)
-    return sequences
-
-  def get_sequence(self, indx, verbosity):
-    node = self.map[indx]
-    if verbosity > 1:
-      print "\tnode:", node
-    parent = node.get_parent()
-    if parent == None: return [indx]
-    else: 
-      parent_indx = parent.get_indx()
-      return self.get_sequence(parent_indx, verbosity) + [indx]
 
 class Node(object):
   """docstring for Node"""
@@ -127,6 +45,97 @@ class Node(object):
       return s_par + "\t|" + s_word + s_prob
     else: return s_word + s_prob
 
+class Graph(object):
+  """docstring for Graph"""
+
+  def __init__(self, max_size=10):
+    self.map = {}
+    self.queue = deque([])
+    self.max_size = max_size
+    self.latest_indx = 0
+
+  def size(self): return len(self.queue)
+  def full(self): return self.size() >= self.max_size
+
+  def add(self, node, parent=None):
+    # increase global index for nodes
+    self.latest_indx +=1
+    node.set_indx(self.latest_indx)
+    if parent is not None:
+      parent = self.map[parent]
+      node.set_parent(parent)
+
+    # print "added", node
+
+    removed = None
+    if self.full():
+      removed = self.queue.popleft()
+      # print "removed", removed, self.map[removed[NI]]
+
+
+    self.map[node.get_indx()] = node
+    self.queue.append((node.get_probability(), node.get_word_indx(), node.get_indx()))
+
+    # if removed EOS check that probability is lower than lowest in current queue. rebuild if not
+    if removed is not None:
+      i = removed[II]
+      p = removed[PI]
+      if i == 2:
+        l = list(self.queue)
+        sorted_queue = sorted(l, key=lambda x: x[PI])
+        lowest_p_in_q = sorted_queue[0][PI]
+        lowest_i_in_q = sorted_queue[0][II]
+        # if p is greater than lowest, rebuild
+        if p >= lowest_p_in_q:
+          new_list = [i for i in self.queue if i[II] != lowest_i_in_q]
+          # del self.map[lowest_i_in_q]
+          # self.map[i]=removed
+          new_list.append(removed)
+          self.queue = deque(new_list)
+
+
+  def print_map(self):
+    # print "graph size:", self.size(), "\t", 
+    for i in self.map: print self.map[i]
+  
+  def print_best(self):
+    # print "graph size:", self.size(), "\t", 
+    for i in self.get_best_queue(): print self.map[i[NI]]
+
+  # returns a sorted list of values
+  def get_best_queue(self): 
+    l = list(self.queue)
+    sor = sorted(l, key=lambda x: x[PI], reverse=True)
+    return sor[:self.max_size]
+  
+  def get_best(self): 
+    best = self.get_best_queue()
+    indices = [i[NI] for i in best if i[II] != 2]
+    word_indices = [i[II] for i in best if i[II] != 2]
+    probabilities = [i[PI] for i in best if i[II] != 2]
+    weights = [self.map[i[NI]].get_weights() for i in best if i[II] != 2]
+    return indices, word_indices, probabilities, weights
+
+  def get_sequences(self, verbosity):
+    sequences = []
+    best = self.get_best_queue()
+    for i in best:
+      if verbosity > 1: print ""
+      sequence = self.get_sequence(i[NI], verbosity)
+      converted_sequence = [self.map[i].get_word_indx() for i in sequence]
+      sequences.append(converted_sequence)
+    return sequences
+
+  def get_sequence(self, indx, verbosity):
+    node = self.map[indx]
+    if verbosity > 1:
+      print "\tnode:", node
+    parent = node.get_parent()
+    if parent == None: return [indx]
+    else: 
+      parent_indx = parent.get_indx()
+      return self.get_sequence(parent_indx, verbosity) + [indx]
+
 
 def en2fr_beam_search(smt, feeder, en_sentence, beam_size, vocab_size, max_search=50, verbosity=0):
 
@@ -154,7 +163,7 @@ def en2fr_beam_search(smt, feeder, en_sentence, beam_size, vocab_size, max_searc
     # print "max_search", max_search
     while j < max_search:
         if verbosity > 1:
-          print ("iter=",j)
+          print "\niter=",j
 
         previous_indices, previous_word_indices, previous_probabilities, previous_weights = nodes.get_best()
         # print "pin", previous_indices
@@ -189,13 +198,14 @@ def en2fr_beam_search(smt, feeder, en_sentence, beam_size, vocab_size, max_searc
 
         # print "pr", np.sort(product_vector[:,:], kind = 'heapsort')[:, -10:][0]
         # print "un", unnormalized_indices
-        # print "ne", new_indices
         # print "pa", parent_rows
 
         probabilities = [post_probability_set[row][0][indx] for row, indx in zip(parent_rows, new_indices)]
         parents = [previous_indices[i] for i in parent_rows]
         parent_probabilities = [previous_probabilities[i] for i in parent_rows]
 
+        # print "pa", parents
+        # print "ne", new_indices
         for i in range(noptions):
             probability = np.log(probabilities[i])
             # +parent_probabilities[i]
